2d39ff8081cd54234bc61f1648681f1ca311816d
caiych <caiych@gmail.com>
2015-10-20 17:01:28 +0800

    Revert "vim -> vim --servername server"
    This reverts commit 740d2bd1d6379ec867e00a4e6f6d794b67fdb619.

--- multi-line.zsh
+++ multi-line.zsh
@@ -1,7 +1,7 @@
 multi_line() {
 	FILENAME=/tmp/multi_line_edit_$(date +"%s").sh
 	echo $LBUFFER | %%location%%/split_lines.py > $FILENAME
-	$EDITOR $FILENAME < /dev/tty
+	vim $FILENAME < /dev/tty
 	LBUFFER=$(%%location%%/join_lines.py < $FILENAME)
 	zle redisplay
 }
